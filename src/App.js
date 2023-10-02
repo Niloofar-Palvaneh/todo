6f380a64014050bf4ac85cd7f7f221e0808e288a
niloofar-palvanehcls <niloofarpalvaneh@gmail.com>
2023-10-02 16:13:42 +0330

    create edit todo section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import EditModal from './components/EditModal/EditModal';
 
 
 function App() {
   const [allTodos, setAllTodos] = useState([])
+  const [isOpenEditModal, setIsOpenEditModal] = useState(false)
+  const [editedTodoId , setEditedTodoId] = useState("")
 
   const getTodos = () => {
     fetch("http://localhost:4000/todos")
@@ -12,10 +15,10 @@ function App() {
       .then(todos => setAllTodos(todos))
   }
 
-  const removeTodo = (id)=>{
-    fetch(`http://localhost:4000/todos/${id}` , {
-      method :"DELETE"
-    }).then(res =>console.log(res))
+  const removeTodo = (id) => {
+    fetch(`http://localhost:4000/todos/${id}`, {
+      method: "DELETE"
+    }).then(res => console.log(res))
     getTodos()
   }
   useEffect(() => {
@@ -23,8 +26,13 @@ function App() {
     getTodos()
   }, [allTodos])
 
-  const doTodoHandler = (id)=>{
+  const doTodoHandler = (id) => {
     console.log(id);
+  }
+
+  const editTodo = (id) => {
+    setIsOpenEditModal(!isOpenEditModal)
+    setEditedTodoId(id)
   }
 
   return (
@@ -37,8 +45,8 @@ function App() {
               <div data-aos="fade-left" className='flex items-center justify-between p-2 border mx-2 '>
                 <div className='flex gap-2'>
                   <input
-                  onClick={()=>doTodoHandler(todo.id)}
-                  type="checkbox" />
+                    onClick={() => doTodoHandler(todo.id)}
+                    type="checkbox" />
                   <p className='font-bold text-gray-600 w-[470px]'>
                     {todo.title}
                   </p>
@@ -53,7 +61,9 @@ function App() {
                     className="w-6 h-6 text-orange-800 transition ease-in-out hover:scale-110 ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
                   </svg>
-                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                  <svg
+                    onClick={() => editTodo(todo.id)}
+                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
                     className="w-6 h-6 text-orange-700 transition ease-in-out hover:scale-110  ">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                   </svg>
@@ -67,7 +77,11 @@ function App() {
           )
         }
       </div>
-
+      {
+        isOpenEditModal && (
+          <EditModal editTodo={editTodo} editedTodoId={editedTodoId} />
+        )
+      }
     </>
   );
 }
--- src/components/EditModal/EditModal.js
+++ src/components/EditModal/EditModal.js
@@ -1,63 +1,60 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-function AddModal({ openModal }) {
-    const [titleTodo, setTitleTodo] = useState("")
-    const [dateTodo, setDateTodo] = useState("")
+function EditModal({ editTodo , editedTodoId }) {
 
+    const [updatedTitle, setUpdatedTitile] = useState("")
+    const [updatedDate, setUpdatedDate] = useState("")
 
-    const setTitleTodoHandler = (event) => {
-        setTitleTodo(event.target.value)
+    const setTitleHandler = (e) => {
+        setUpdatedTitile(e.target.value)
     }
-    const setDateTodoHandler = (event) => {
-        setDateTodo(event.target.value)
+    const setDateHandler = (e) => {
+        setUpdatedDate(e.target.value)
     }
-
-    const submitDateAddModal = () => {
-        const newTodo = {
-            title: titleTodo,
-            date: dateTodo,
-            isCompleted : false
-        }
-        fetch("http://localhost:4000/todos", {
-            method: "POST",
+    const editTodoHandler = ()=>{
+        fetch(`http://localhost:4000/todos/${editedTodoId}` , {
+            method : "PUT",
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(newTodo)
+            body: JSON.stringify({
+                title : updatedTitle,
+                date : updatedDate,
+            })
         })
-        openModal()
+        editTodo()
     }
-
 
 
     return (
         <>
             <div className='w-full h-screen bg-blue-c absolute top-0 left-0 flex items-center justify-center z-50 '>
                 <div className='bg-white flex flex-col gap-4 p-4 rounded shadow-xl'>
-                    <h3 className='text-xl font-bold text-gray-500'>Add Task</h3>
+                    <h3 className='text-xl font-bold text-gray-500'>Edit Todo</h3>
                     <div className='flex gap-2 sm:flex-col sm:gap-4'>
                         <div className='flex flex-col items-start w-[200px] '>
                             <label htmlFor="title" className='text-gray-500'>Task</label>
                             <input
-                                onChange={setTitleTodoHandler}
-                                value={titleTodo} type="text" name='title' placeholder='learning...' className='outline-none border p-2 rounded' />
+                                onChange={setTitleHandler}
+                                value={updatedTitle} type="text" name='title' placeholder='learning...' className='outline-none border p-2 rounded' />
                         </div>
                         <div className='flex flex-col items-start w-[200px] '>
                             <label htmlFor="date" className='text-gray-500'>Date</label>
                             <input
-                                onChange={setDateTodoHandler}
-                                value={dateTodo} type="text" name='date' placeholder='may/5/2023..' className='outline-none border p-2 rounded' />
+                                onChange={setDateHandler}
+                                value={updatedDate}
+                                type="text" name='date' placeholder='may/5/2023..' className='outline-none border p-2 rounded' />
                         </div>
                     </div>
                     <div className='flex gap-2 items-center justify-end'>
                         <button
-                            onClick={submitDateAddModal}
+                        onClick={editTodoHandler}
                             className='bg-orange-500 text-white p-1 rounded shadow border border-orange-500 w-[80px]
                               transition ease-in-out hover:bg-white hover:text-orange-500'>
-                            Add
+                            Edit
                         </button>
                         <button
-                            onClick={openModal}
+                            onClick={editTodo}
                             className='bg-orange-500 text-white p-1 rounded shadow border border-orange-500 w-[80px]
                                transition ease-in-out hover:bg-white hover:text-orange-500'>
                             Cancel
@@ -70,4 +67,4 @@ function AddModal({ openModal }) {
     );
 }
 
-export default AddModal;
+export default EditModal;
